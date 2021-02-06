05e30e1cea74c58e06d2fe02ca2ffa270fb59ee0
Emewjin <yujinwork03@gmail.com>
2021-02-06 15:45:34 +0900

    비디오 추가 코드 수정

--- video.js
+++ video.js
@@ -1,8 +1,8 @@
 const videoBox = document.querySelector('.video-box');
 
 const videoArray = [
-    { title: 'test1', url: 'test1', year: 'test1', season: 'test1' },
-    { title: 'test2', url: 'test2', year: 'test2', season: 'test2' },
+    { title: '결승', url: 'sHRKL8ZZZbQ', year: '2020', season: '시즌1' },
+    { title: '결승', url: 'Arpjtc5F9bU', year: '2020', season: '시즌2' },
 ];
 
 function appendHtml(videoTag) {
@@ -11,33 +11,24 @@ function appendHtml(videoTag) {
     videoBox.appendChild(div);
 }
 
-function createVideoTag(videoTitle, videoURL){
+function createVideoTag(videoTitle, videoURL, playYear, playSeason){
     let videoTag = 
-    `<div class="video"><span>${videoTitle}</span><div class="video-container"><iframe width="100%" height="100%" src="${videoURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="video-tags"><button>태그</button></div></div>`;
+    `<div class="video"><span>${videoTitle}</span><div class="video-container"><iframe width="100%" height="100%" src="${videoURL}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><div class="video-tags"><button>${playYear}${playSeason}</button></div></div>`;
     appendHtml(videoTag); 
 }
 
 function createInfo(){
     i = 0 ;
-    while(i < videoArray.length){
+    while (i < videoArray.length){
     const videoTitle = videoArray[i].title;
     const videoURL =  `https://www.youtube.com/embed/${videoArray[i].url}`;
-    createVideoTag(videoTitle, videoURL);
-    i = i + 1;
+    const playYear = videoArray[i].year;
+    const playSeason = videoArray[i].season;
+
+    createVideoTag(videoTitle, videoURL, playYear, playSeason);
+    i = i + 1 ;
     }
+
 }
 
-videoArray.forEach(createInfo);
-
-
-
-
-
-
-// function appendHtml(el, str){
-//     const div = document.createElement('div');
-//     div.innerHTML = str;
-//     while (div.children.length > 0) {
-//     el.appendChild(div.children[0]);
-//     }
-// }
+createInfo();
