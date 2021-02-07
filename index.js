331e0271ba67bbb75e48670e46a182ca08c4d02e
Emewjin <yujinwork03@gmail.com>
2021-02-07 23:25:40 +0900

    메뉴 열렸을 때 아이콘 X로 변경

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const menuIcon = menu.querySelector('i');
 const dropMenu = document.querySelector('.drop-content');
 const SHOWING = "showing";
 const topBtn = document.querySelector('.go-top');
+const timesIcon = "fa-times"
 
 function scrollTop(){
     window.scrollTo(0,0);
@@ -11,6 +12,17 @@ topBtn.addEventListener("click", scrollTop);
 
 function showMenu(){
     dropMenu.classList.toggle(SHOWING);
+    menuIcon.classList.toggle(timesIcon);
 }
 menuIcon.addEventListener("click",showMenu);
+
+// function updateToggle(){
+//     if (this.paused) {
+//         // console.log('play');
+//         toggle.textContent = '►';
+//     } else {
+//         // console.log('pause');
+//         toggle.textContent = '❚❚';
+//     }
+// }
 
